3ce9ebcf3af1aebcf15251313d9819af5f7436af
Qazi Ahmad <107860183+QaziAhmad77@users.noreply.github.com>
2023-02-13 22:16:06 +0500

    Update useState.js

--- src/component/Hooks/useState.js
+++ src/component/Hooks/useState.js
@@ -14,10 +14,6 @@ const UseState = (props) => {
         <p className={`text-${props.mode === "black" ? "black" : "white"} text-center display-4 mt-3`}>{myNum}</p>
         <div className={`d-flex justify-content-center align-items-center`}>
           <div className="button2" onClick={() => setMyNum(myNum + 1)}>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
             INCR
           </div>
           <div className="button2" onClick={() => (myNum > 0 ? setMyNum(myNum - 1) : setMyNum(0))}>
